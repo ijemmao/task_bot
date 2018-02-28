086f49b8644c38b231269a573b71fcd7dd5d0b91
Ijemma Onwuzulike <ijemmaonwuzulike10@gmail.com>
2018-02-28 12:07:15 -0500

    bot listens for comments and stores messages with timestamps

--- app/server.js
+++ app/server.js
@@ -1,11 +1,15 @@
 import botkit from 'botkit'
 
-console.log('starting bot');
+console.log('starting bot')
+
+let channelMessages = {
+
+}
 
 // botkit controller
 const controller = botkit.slackbot({
   debug: false,
-});
+})
 
 // initialize slackbot
 const slackbot = controller.spawn({
@@ -14,16 +18,33 @@ const slackbot = controller.spawn({
 }).startRTM(err => {
   // start the real time message client
   if (err) {
-    throw new Error(err);
+    throw new Error(err)
   }
-});
+})
 
 // prepare webhook
 // for now we won't use this but feel free to look up slack webhooks
 controller.setupWebserver(process.env.PORT || 3001, (err, webserver) => {
   controller.createWebhookEndpoints(webserver, slackbot, () => {
     if (err) {
-      throw new Error(err);
+      throw new Error(err)
     }
-  });
-});
+  })
+})
+
+
+// listens for any comment made by user
+// messages stored in dictionary that holds:
+//    1. user id
+//    2. message timestamp
+controller.hears([''], ['direct_message'], (bot, message) => {
+  bot.api.users.info({ user: message.user }, (err, res) => {
+    if (err) return err
+    if (!channelMessages[res.user.id]) {
+      channelMessages[res.user.id] = { messageTimes: [] }
+    }
+    if (res) {
+      channelMessages[res.user.id].messageTimes.push(message.ts)
+    }
+  })
+})
