bc95822a13d69e85b8290083e96316352557da7e
Sofia Stanescu-Bellu <sofiastb27@gmail.com>
2018-02-28 17:02:58 -0500

    Schedule task

--- app/server.js
+++ app/server.js
@@ -1,10 +1,16 @@
 import botkit from 'botkit'
+import schedule from 'node-schedule';
+
+var j = schedule.scheduleJob('42 * * * *', function(){
+  console.log('The answer to life, the universe, and everything!');
+});
 
 console.log('starting bot');
 
 // botkit controller
 const controller = botkit.slackbot({
-  debug: false,
+  debug: false, 
+
 });
 
 // initialize slackbot
@@ -19,11 +25,28 @@ const slackbot = controller.spawn({
 });
 
 // prepare webhook
-// for now we won't use this but feel free to look up slack webhooks
 controller.setupWebserver(process.env.PORT || 3001, (err, webserver) => {
   controller.createWebhookEndpoints(webserver, slackbot, () => {
     if (err) {
       throw new Error(err);
     }
   });
+});
+
+var rule = new schedule.RecurrenceRule();
+rule.dayOfWeek = 3;
+rule.hour = 17;
+rule.minute = 0;
+ 
+var milestones = schedule.scheduleJob(rule, function(){
+  slackbot.say( {
+      text: 'It\'s milestone time!',
+      channel: 'C9G073P2N'
+    }
+  );
+});
+
+// example hello response
+controller.hears(['date'], ['direct_message'], (bot, message) => {
+  bot.reply(message, "The date today is " + day + " " + time);
 });
