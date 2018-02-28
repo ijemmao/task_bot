0aa2c9567f067d23055e0061e8ccb2ce0e757247
Ijemma Onwuzulike <ijemmaonwuzulike10@gmail.com>
2018-02-27 22:08:03 -0500

    changed token variable

--- app/server.js
+++ app/server.js
@@ -1,3 +1,29 @@
-// example bot
+import botkit from 'botkit'
 
 console.log('starting bot');
+
+// botkit controller
+const controller = botkit.slackbot({
+  debug: false,
+});
+
+// initialize slackbot
+const slackbot = controller.spawn({
+  token: process.env.TASK_BOT_TOKEN,
+  // this grabs the slack token we exported earlier
+}).startRTM(err => {
+  // start the real time message client
+  if (err) {
+    throw new Error(err);
+  }
+});
+
+// prepare webhook
+// for now we won't use this but feel free to look up slack webhooks
+controller.setupWebserver(process.env.PORT || 3001, (err, webserver) => {
+  controller.createWebhookEndpoints(webserver, slackbot, () => {
+    if (err) {
+      throw new Error(err);
+    }
+  });
+});
