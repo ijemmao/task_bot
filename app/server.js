65534e749e44031a05436a937a332ce00abd13e1
Sofia Stanescu-Bellu <sofiastb27@gmail.com>
2018-02-28 18:25:20 -0500

    Post message in pre-defined channel list

--- app/server.js
+++ app/server.js
@@ -1,21 +1,16 @@
 import botkit from 'botkit'
 import schedule from 'node-schedule';
 
-var j = schedule.scheduleJob('42 * * * *', function(){
-  console.log('The answer to life, the universe, and everything!');
-});
-
 console.log('starting bot');
 
 // botkit controller
 const controller = botkit.slackbot({
   debug: false, 
-
 });
 
 // initialize slackbot
 const slackbot = controller.spawn({
-  token: process.env.TASK_BOT_TOKEN,
+  token: process.env.SLACK_BOT_TOKEN,
   // this grabs the slack token we exported earlier
 }).startRTM(err => {
   // start the real time message client
@@ -33,20 +28,22 @@ controller.setupWebserver(process.env.PORT || 3001, (err, webserver) => {
   });
 });
 
+/************************* milestones *************************/
+
+var channels = ['C9G17060H', 'C9G073P2N'];  // dummy channel list
+
+// schedule milestone message for Wednesday after the Lab meeting
 var rule = new schedule.RecurrenceRule();
 rule.dayOfWeek = 3;
-rule.hour = 17;
-rule.minute = 0;
+rule.hour = 18;
+rule.minute = 23;
+rule.second = 10;
  
 var milestones = schedule.scheduleJob(rule, function(){
-  slackbot.say( {
+  for (var i = 0; i < channels.length; i++) {
+    slackbot.say( {
       text: 'It\'s milestone time!',
-      channel: 'C9G073P2N'
-    }
-  );
-});
-
-// example hello response
-controller.hears(['date'], ['direct_message'], (bot, message) => {
-  bot.reply(message, "The date today is " + day + " " + time);
+      channel: channels[i]
+    });
+  }
 });
