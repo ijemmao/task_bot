dfdf6381c7a36a1ab461c2712b7e9b5482c972e3
Sofia Stanescu-Bellu <sofiastb27@gmail.com>
2018-03-02 22:18:14 -0500

    Curate all 9 milestones, schedule hebdomadal occurance between a given start and end date

--- app/server.js
+++ app/server.js
@@ -1,12 +1,13 @@
-import botkit from 'botkit';
-import schedule from 'node-schedule';
-import data from '../mock_data/milestones';
-import * as markdown from "./markdown.js";
+import botkit from 'botkit'
+import schedule from 'node-schedule'
+import data from '../mock_data/milestones' 
+import moment from 'moment'
+import * as markdown from './markdown.js' 
 
 // botkit controller
 const controller = botkit.slackbot({
   debug: false, 
-});
+}) 
 
 // initialize slackbot
 const slackbot = controller.spawn({
@@ -15,46 +16,75 @@ const slackbot = controller.spawn({
 }).startRTM(err => {
   // start the real time message client
   if (err) {
-    throw new Error(err);
+    throw new Error(err) 
   }
-});
+}) 
 
 // prepare webhook
 controller.setupWebserver(process.env.PORT || 3001, (err, webserver) => {
   controller.createWebhookEndpoints(webserver, slackbot, () => {
     if (err) {
-      throw new Error(err);
+      throw new Error(err) 
     }
-  });
-});
+  }) 
+}) 
 
 /************************* milestones *************************/
 
-var channels = ['C9G17060H', 'C9G073P2N'];  // dummy channel list
-var milestone = '_It\'s milestone time!_\n\n' // create milestone message.
-              + markdown.formatHeader(Object.keys(data.milestones.one)[0]) 
-              + data.milestones.one.Overview + '\n'
-              + markdown.formatHeader(Object.keys(data.milestones.one)[1]) 
-              + '\n' + markdown.formatLists(data.milestones.one.Everyone) 
-              + markdown.formatHeader(Object.keys(data.milestones.one)[2])
-              + markdown.formatLists(data.milestones.one.PM)
-              + markdown.formatHeader(Object.keys(data.milestones.one)[3])
-              + markdown.formatLists(data.milestones.one.Deliverables)
-              + ('\n*' + data.milestones.one.More + "*");
+var channels = ['C9G17060H', 'C9G073P2N']   // dummy channel list
 
-// schedule milestone message for Wednesday after the Lab meeting
-var rule = new schedule.RecurrenceRule();
-rule.dayOfWeek = 3;
-rule.hour = 21;
-rule.minute = 46;
-rule.second = 30;
+// start and end dates for term and week count
+var startDate = moment('03/28/2018', 'MM/DD/YYYY')
+var endDate = moment('05/30/2018', 'MM/DD/YYYY')
+var currentWeek = 0
 
-// post milestones message in each channel in channels list 
-var milestones = schedule.scheduleJob(rule, function(){
-  for (var i = 0; i < channels.length; i++) {
-    slackbot.say( {
-      text: milestone,
-      channel: channels[i]
-    });
+// schedule milestone message for Wednesday after the Lab meeting at 8pm
+var rule = new schedule.RecurrenceRule() 
+rule.dayOfWeek = 3
+rule.hour = 20
+rule.minute = 0
+rule.second = 0 
+
+// if the current date is within the start and end date range
+if (moment().isSameOrAfter(startDate) && moment().isSameOrBefore(endDate) && currentWeek <= 8) {
+  
+  // post milestones message in each channel in channels list if it's the corresponding time
+  var milestones = schedule.scheduleJob(rule, function(){
+    for (var i = 0; i < channels.length;  i++) {
+      slackbot.say( {
+        text: getMilestone(currentWeek),
+        channel: channels[i]
+      }) 
+    }
+    currentWeek++
+  }) 
+}
+
+/*
+* getMilestone() takes in the current week number and gets and formats the milestone for
+* that week.
+*/
+function getMilestone(week) {
+  var raw_milestone = data[week] // pulls raw milestone
+  var milestone = '_It\'s milestone time!_\n\n' 
+
+  // adds weekly milestone title and week #
+  milestone += '*Week ' + raw_milestone['milestone'] + ':* ' + (raw_milestone['title'] + '\n')
+
+  // formats each section of the milestone in markdown format
+  for (var section in raw_milestone) {
+    if (section != 'milestone' && section != 'title') {
+      milestone += markdown.formatHeader(section) 
+      if (typeof raw_milestone[section] === typeof new Object()) {
+        milestone += markdown.formatLists(raw_milestone[section]) 
+      } else {
+        milestone += (raw_milestone[section] + '\n')
+      }
+    }
   }
-});
+
+  // tacks on the 'But wait, there's more!' statement
+  milestone += "\nBut wait, there's more! Check out https://build.dali.dartmouth.edu for the full list of tasks." 
+
+  return milestone
+}
