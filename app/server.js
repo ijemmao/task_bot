e9f82d6ec16f6765c867d5359ec495baf6660c5b
Ijemma Onwuzulike <ijemmaonwuzulike10@gmail.com>
2018-03-03 03:32:23 -0500

    correct eslint errors

--- app/server.js
+++ app/server.js
@@ -10,8 +10,8 @@ let channelMessages = []
 
 // botkit controller
 const controller = botkit.slackbot({
-  debug: false, 
-}) 
+  debug: false,
+})
 
 let channelMessages = []
 
@@ -22,18 +22,18 @@ const slackbot = controller.spawn({
 }).startRTM(err => {
   // start the real time message client
   if (err) {
-    throw new Error(err) 
+    throw new Error(err)
   }
-}) 
+})
 
 // prepare webhook
 controller.setupWebserver(process.env.PORT || 3001, (err, webserver) => {
   controller.createWebhookEndpoints(webserver, slackbot, () => {
     if (err) {
-      throw new Error(err) 
+      throw new Error(err)
     }
-  }) 
-}) 
+  })
+})
 
 /*
  * Listens for any comment from any channel
@@ -55,71 +55,83 @@ controller.on(['ambient', 'direct_message', 'file_share'], (bot, message) => {
   })
 })
 
-/*
- * Listens for database printout
- */
+controller.hears('add_user', ['ambient'], (bot, message) => {
+  bot.api.users.info({ user: message.user }, (err, res) => {
+    const newUser = new User({
+      id: res.user.id,
+      username: res.user.name,
+      firstName: res.user.profile.first_name,
+      lastName: res.user.profile.last_name,
+    })
+    newUser.save((err, response) => {
+      if (err) return err
+      bot.reply(message, `Added ${res.user.name} to the database`)
+    })
+  })
+})
+
 controller.hears(['show'], ['direct_message'], (bot, message) => {
   console.log(channelMessages)
 })
 
 /************************* milestones *************************/
 
-var channels = ['C9G17060H', 'C9G073P2N']   // dummy channel list
-
-// start and end dates for term and week count
-var startDate = moment('03/28/2018', 'MM/DD/YYYY')
-var endDate = moment('05/30/2018', 'MM/DD/YYYY')
-var currentWeek = 0
-
-// schedule milestone message for Wednesday after the Lab meeting at 8pm
-var rule = new schedule.RecurrenceRule() 
-rule.dayOfWeek = 3
-rule.hour = 20
-rule.minute = 0
-rule.second = 0 
-
-// if the current date is within the start and end date range
-if (moment().range(startDate, endDate) && currentWeek <= 8) {
-  
-  // post milestones message in each channel in channels list if it's the corresponding time
-  schedule.scheduleJob(rule => {
-    channels.forEach(channel => {
-      slackbot.say({
-        text: getMilestone(currentWeek),
-        channel: channel
-      }) 
-    })
-    currentWeek += 1
-  })
-}
-
 /*
 * getMilestone() takes in the current week number and gets and formats the milestone for
 * that week.
 */
-let getMilestone = (week) => {
-  var raw_milestone = data[week] // pulls raw milestone
-  var milestone = '@channel _It\'s milestone time!_\n\n' 
+const getMilestone = (week) => {
+  const rawMilestone = data[week] // pulls raw milestone
+  let milestone = '@channel _It\'s milestone time!_\n\n'
 
   // adds weekly milestone title and week #
-  milestone += '*Week ' + raw_milestone['milestone'] + ':* ' + (raw_milestone['title'] + '\n')
+  milestone += `*Week ${rawMilestone.milestone}:* ${rawMilestone.title}\n`
 
   // formats each section of the milestone in markdown format
-  for (var section in raw_milestone) {
+  for (const section in rawMilestone) {
     if (section != 'milestone' && section != 'title') {
       milestone += markdown.formatHeader(section) 
-      if (typeof raw_milestone[section] === typeof new Object()) {
-        milestone += markdown.formatLists(raw_milestone[section]) 
+      if (typeof rawMilestone[section] === typeof new Object()) {
+        milestone += markdown.formatLists(rawMilestone[section])
       } else {
-        milestone += (raw_milestone[section] + '\n')
+        milestone += (`${rawMilestone[section]}\n`)
       }
     }
   }
 
   // tacks on the 'But wait, there's more!' statement
-  milestone += "\nBut wait, there's more! Check out https://build.dali.dartmouth.edu for the full list of tasks." 
+  milestone += '\nBut wait, there\'s more! Check out https://build.dali.dartmouth.edu for the full list of tasks.'
 
   return milestone
+}
+
+const channels = ['C9G17060H', 'C9G073P2N']   // dummy channel list
+
+// start and end dates for term and week count
+const startDate = moment('03/28/2018', 'MM/DD/YYYY')
+const endDate = moment('05/30/2018', 'MM/DD/YYYY')
+let currentWeek = 0
+
+// schedule milestone message for Wednesday after the Lab meeting at 8pm
+const rule = new schedule.RecurrenceRule()
+rule.dayOfWeek = 3
+rule.hour = 20
+rule.minute = 0
+rule.second = 0
+
+// if the current date is within the start and end date range
+if (moment().range(startDate, endDate) && currentWeek <= 8) {
+
+  // post milestones message in each channel in channels list if it's the corresponding time
+  schedule.scheduleJob(r => {
+    channels.forEach(channel => {
+      slackbot.say({
+        text: getMilestone(currentWeek),
+        channel,
+      })
+      currentWeek += 1
+    })
+  })
 }
 
 controller.hears('add_user', ['direct_message'], (bot, message) => {
