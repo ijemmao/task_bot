32bc5ee139bdd9e0b2e0cc019fc01e9acfd7a434
Ijemma Onwuzulike <ijemmaonwuzulike10@gmail.com>
2018-03-19 18:43:04 -0700

    created team model

---
+++ app/models/team.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose'
+const Schema = mongoose.Schema
+
+const teamSchema = new Schema({
+  name: String,
+  meetingTimes: [Date],
+  projectManager: String,
+  developers: [String],
+  designers: [String],
+})
+
+const team = mongoose.model('Team', teamSchema)
+module.export = team
